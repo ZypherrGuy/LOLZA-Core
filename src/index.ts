c07e19396f6dc6e0f69324110bce7c4929b6d275
Kyle Macleod <kds.gametech@gmail.com>
2024-11-05 19:07:33 +0200

    dev/Changes Ports to test

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.get("/api/ping", (req, res) => {
   res.json({ message: "pong" });

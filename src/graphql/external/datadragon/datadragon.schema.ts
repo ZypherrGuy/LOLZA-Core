5daa400eaf5e6327e3d1b435d298497da7315944
Kyle Macleod <kds.gametech@gmail.com>
2025-02-15 12:03:45 +0200

    dev/adding images

--- src/graphql/external/datadragon/datadragon.schema.ts
+++ src/graphql/external/datadragon/datadragon.schema.ts
@@ -1,11 +1,22 @@
 import { gql } from 'apollo-server-express';
 
 export const dataDragonTypeDefs = gql`
+  type ChampionImage {
+    full: String!
+    sprite: String!
+    group: String!
+    x: Int!
+    y: Int!
+    w: Int!
+    h: Int!
+  }
+
   type Champion {
     id: String!
     key: String!
     name: String!
     title: String!
+    image: ChampionImage!
   }
 
   extend type Query {
